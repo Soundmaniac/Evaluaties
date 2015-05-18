efb12553e6a300b792191c4dc37a52ba25e4372a
Barry Stavenuiter <witkop525@hotmail.com>
2015-05-18 14:42:51 +0200

    Evaluatie insert aangepast
    Een cursist met "zelfde" cursistID kan nu eenmalig een evaluatie
    forumlier posten (dus niet meerdere keren).

--- insertReview.php
+++ insertReview.php
@@ -9,6 +9,8 @@ OpenConnection();
 * $row = mysql_fetch_assoc($result);
 */
 
+/*Every specific column can be handled differently when there is not value being posted
+by replacing the null value in the following part of the code:*/
 $cursistID = $_POST['id'];
 $cursusinhouda = (isset($_POST['group1a']) ? $_POST['group1a'] : null);
 $cursusinhoudb = (isset($_POST['group1b']) ? $_POST['group1b'] : null);
@@ -21,15 +23,25 @@ $trainerb = (isset($_POST['group4b']) ? $_POST['group4b'] : null);
 $trainercomm = (isset($_POST['txtarea2']) ? mysql_real_escape_string(htmlentities($_POST['txtarea2'])) : null);
 $algemeenoordeela = (isset($_POST['group5']) ? $_POST['group5'] : null);
 $algemeenoordeelcomm =(isset($_POST['txtarea3']) ? mysql_real_escape_string(htmlentities($_POST['txtarea3'])) : null);
+/*End of setting variables*/
 
+$query1 = "SELECT cursistID FROM ttsurveyresults WHERE cursistID = '" . $cursistID . "'";
+$query2 = "INSERT INTO ttsurveyresults(cursistID, cursusinhouda, cursusinhoudb, cursusinhoudc, cursusinhoudcomm, structuura, cursustmateriaala, trainera, trainerb, trainercomm, algemeenoordeela, algemeenoordeelcomm) VALUES('$cursistID', '$cursusinhouda', '$cursusinhoudb', '$cursusinhoudc', '$cursusinhoudcomm', '$structuura', '$cursustmateriaala', '$trainera', '$trainerb', '$trainercomm', '$algemeenoordeela', '$algemeenoordeelcomm')";
 
-$selectstring1 = "INSERT INTO ttsurveyresults(cursistID, cursusinhouda, cursusinhoudb, cursusinhoudc, cursusinhoudcomm, structuura, cursustmateriaala, trainera, trainerb, trainercomm, algemeenoordeela, algemeenoordeelcomm) VALUES('$cursistID', '$cursusinhouda', '$cursusinhoudb', '$cursusinhoudc', '$cursusinhoudcomm', '$structuura', '$cursustmateriaala', '$trainera', '$trainerb', '$trainercomm', '$algemeenoordeela', '$algemeenoordeelcomm')";
-
-$sql = mysql_query($selectstring1);
-if(!$sql)
+if(mysql_num_rows(mysql_query($query1)) > 0)
 {
-    echo("Could not run query: " . mysql_error());
-    exit;
+	echo("De resultaten van de persoon met cursistID " . $cursistID . " bestaan al.");
+	/*Wat hierna gebeurd kun je eventueel aanpassen*/
+	exit;
+}
+else
+{
+	$sql = mysql_query($query2);
+	if(!$sql)
+	{
+		echo("Could not run query: " . mysql_error());
+		exit;
+	}
 }
 
 CloseConnection();
