70091efa6d0c7ea98b97b9b5fbea729e2056ef48
Barry Stavenuiter <witkop525@hotmail.com>
2015-07-23 20:52:58 +0200

    Login pagina opmaak aangepast

--- index.php
+++ index.php
@@ -9,39 +9,26 @@ if(isset($_SESSION['gebruiker']))
 
 <html>
 	<head>
-
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title></title>
-
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>PCI Languages - Evaluatie login</title>
 	</head>
 	<body>
-
 		<div id="login">
-
-			
+			<h2>Login op PCI Languages - evaluaties</h2>
 			<form action="login.php" method="post">
-
-				<div class="center" style="background-color: rgb(255, 255, 255); border-radius: 5px; box-shadow: 2px 2px 8px #444444;">
-
-					<table class="tcenter">
-						<tr>
-							<td class="center" style="border-bottom: 0;">Gebruikersnaam: </td> <td class="center" style="border-bottom: 0;"><input type="text" name="gebruikersnaam"></td>
-						</tr>
-						<tr>
-							<td class="center" style="border-bottom: 0;">Wachtwoord: </td> <td class="center" style="border-bottom: 0;"><input type="password" name="wachtwoord"></td>
-						</tr>
-					</table>
-					<div class="clear"> </div>
-					
-					<input style="margin-bottom: 25px;" type="submit" name="inloggen" value="Inloggen" class="submit"/>
-					
-					<input style="margin-bottom: 25px;" type="submit" name="registeren" value="Registreren" class="submit"/>
-
-					<div class="clear"> </div>
-				</div>
-			
+				<table class="tcenter">
+					<tr>
+						<td class="center" style="border-bottom: 0;">Gebruikersnaam: </td> <td class="center" style="border-bottom: 0;"><input type="text" name="gebruikersnaam"></td>
+					</tr>
+					<tr>
+						<td class="center" style="border-bottom: 0;">Wachtwoord: </td> <td class="center" style="border-bottom: 0;"><input type="password" name="wachtwoord"></td>
+					</tr>
+				</table>
+				<div class="clear"> </div>
+				<input type="submit" name="inloggen" value="Inloggen" />
+				<input type="submit" name="registreren" value="Registreren" />
+				<div class="clear"> </div>
 			</form>		
 		</div>
-		
 	</body>
 </html>
